6e4520ec15bf3682418e230e0d94671c8f068a59
yoratoni <71235233+yoratoni@users.noreply.github.com>
2023-10-17 17:25:38 +0200

    WIP

--- src/lib/crypto/sha256.ts
+++ src/lib/crypto/sha256.ts
@@ -1,13 +1,15 @@
 /*
  * A GPU accelerated TypeScript implementation of the Secure Hash Algorithm, SHA-256, as defined
  * in FIPS 180-2.
+ *
  * Version 2.2 Copyright Angel Marin, Paul Johnston 2000 - 2009.
  * Other contributors: Greg Holt, Andrew Kepert, Ydnar, Lostinet.
  * Distributed under the BSD License.
  * See http://pajhome.org.uk/crypt/md5 for details.
  * Also http://anmar.eu.org/projects/jssha2/
  *
- * Based on the SHA256 pseudocode implementation
+ * Based on the SHA256 pseudocode from Wikipedia.
  * See https://en.wikipedia.org/wiki/SHA-2
+ * Also https://csrc.nist.gov/files/pubs/fips/180-4/final/docs/fips180-4.pdf
  */
 
